75dcedad0bee92902ca41c56d42ddf418ccfba10
Dmitriy <99681039+RayzOnePy@users.noreply.github.com>
2023-10-17 13:28:20 +0700

    task5

---
+++ project/week1/basics/task5/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$content = '<h1>Супер</h1><p>соник</p>';
+$header = "ОБЬЕМНО";
+$sidebar = "Очень";
+$footer = "фулл в тг";
+
+require __DIR__ . '/header.php';
+require __DIR__ . '/sidebar.php';
+require __DIR__ . '/content.php';
+require __DIR__ . '/footer.php';
