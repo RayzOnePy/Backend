d37c9e8614a22b681f32cdcb14e0e9428ecf8243
Dmitriy <99681039+RayzOnePy@users.noreply.github.com>
2023-10-20 16:47:58 +0700

    task3

--- project/week1/oop/task2/index.php
+++ project/week1/oop/task2/index.php
@@ -5,7 +5,7 @@
         private string $title;
         private string $text;
 
-        public function getTitle() : string
+        public function getTitle(): string
         {
             return $this->title;
         }
@@ -15,7 +15,7 @@
             $this->title = $title;
         }
 
-        public function getText() : string
+        public function getText(): string
         {
             return $this->text;
         }
@@ -57,12 +57,12 @@
     {
         private float $price;
 
-        public function setPrice(float $price) : void
+        public function setPrice(float $price): void
         {
             $this->price = $price;
         }
 
-        public function getPrice() : string
+        public function getPrice(): string
         {
             return $this->price;
         }
@@ -70,10 +70,9 @@
         public function __construct(string $title, string $text, string $homework, float $price)
         {
             parent::__construct($title, $text, $homework);
-            $this->price = $price;            
+            $this->price = $price;
         }
     }
 
     $paidLesson = new PaidLesson('Урок о наследовании в PHP', 'Лол, кек, чебурек', 'Ложитесь спать, утро вечера мудренее', 99.90);
     var_dump($paidLesson);
-?>
